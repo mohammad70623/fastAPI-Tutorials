271545bcd7ce7462faa631ced175da233484eeff
mohamad70623 <mohammad70623@gmail.com>
2025-12-29 21:03:11 +0600

    json file data loaded

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI 
+import json
 
-app = FastAPI() 
+app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
