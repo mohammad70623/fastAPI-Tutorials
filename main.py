8b8ede50aeee87b05ab35d095c48693ad2521032
mohamad70623 <mohammad70623@gmail.com>
2025-12-30 08:21:39 +0600

    path parameter is done

--- main.py
+++ main.py
@@ -20,3 +20,13 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str):
+    #load all the patient 
+    data = load_data() 
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'Error':'patient does not found'}
+
