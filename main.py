886be725b843dd6ad6ee70f73543ca088e89e124
mohamad70623 <mohammad70623@gmail.com>
2026-01-01 23:53:23 +0600

    Patient update pydanric model added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 app = FastAPI()
@@ -34,6 +34,14 @@ class Patient(BaseModel):
             return 'OverWeight'
         else:
             return 'Obese'
+        
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender:Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height:Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 
 def load_data():
