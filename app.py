b5e6f9c4ef6cbbdcc3fc9e9355ddec8e6fc28f32
mohamad70623 <mohammad70623@gmail.com>
2026-01-03 00:06:07 +0600

    pydantic model is created

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, computed_field, Field 
-from typing import Literal 
+from typing import Literal, Annotated
 import pickle 
 import pandas as pd
 
 #import the ML model 
-with open('fraud detection pipeline.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
+app = FastAPI()
+
+#pudantic model to validate incoming data
+class UserInput(BaseModel):
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='weight of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary in LPA' )]
+    smoker: Annotated[bool, Field(..., description='is user is smoker')]
+    city:  Annotated[str, Field(..., description='the city of users belongs to')]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job', 'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the users')]
+
+
+
+    
